c6a48825be9ccbdd197ddfd0cb72ef1c66b06d96
selvamsivalingam123 <selvamsivalingam123@gmail.com>
2023-02-08 08:57:57 +0530

    First Commit

---
+++ EmployeeDetails.java
@@ -0,0 +1,25 @@
+package com.polymorphism;
+
+public class EmployeeDetails {  //parent
+
+	public void empName() {
+		System.out.println("Employee name is Aravind");
+	}
+
+	public void empId(int a ) {
+		System.out.println("Employee ID is "+a);
+	}
+	
+	public void test(String msg) {
+		System.out.println(msg);
+	}
+	
+	public static void main(String[] args) {
+		EmployeeDetails e = new Sample(); //UPcasting
+		e.empName();
+		e.empId(600);
+		e.test("JAVA");
+
+	}
+
+}
